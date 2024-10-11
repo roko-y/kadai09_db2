d0d33adadc98e765a4d88202fc22989d41902336
roko-y <yasukokomoro@yahoo.co.jp>
2024-10-11 23:03:46 +0900

    やっと提出

---
+++ update.php
@@ -0,0 +1,28 @@
+<?php
+//1. POSTデータ取得
+$bookname   = $_POST["bookname"];
+$bookurl    = $_POST["bookurl"];
+$bookcomment = $_POST["bookcomment"];
+$id         = $_POST["id"];
+
+
+//2. DB接続します
+include("funcs.php");
+$pdo = db_conn();
+
+//３．データ登録SQL作成
+$sql = "UPDATE gs_bm_table SET bookname=:bookname,bookurl=:bookurl,bookcomment=:bookcomment WHERE id=:id";
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':bookname',    $bookname,    PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':bookurl',     $bookurl,     PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':bookcomment', $bookcomment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':id',          $id,          PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute(); //実行
+
+//４．データ登録処理後
+if($status==false){
+  sql_error($stmt);
+}else{
+  redirect("select.php");
+}
+?>
